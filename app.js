a4e37fccec14f3f3462dd5e54d771302cc15e6ce
Purushothaman G <officialpurushothaman@gmail.com>
2023-05-11 13:30:24 +0530

    pott

--- app.js
+++ app.js
@@ -100,5 +100,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(port, function () {
-  console.log(`Started building at ${port}`);
+  console.log(`Started building at ${process.env.PORT || port}`);
 });
